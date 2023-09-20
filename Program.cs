1ce471ae202d70d1540daa77345f53138db22e42
Andrew Monteith <339531824@GoTVDSB.ca>
2023-09-20 10:48:27 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_6___Looping_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string menuOption;
+                Console.Clear();
+                Console.WriteLine("Press 1 for Prompter");
+                Console.WriteLine("Press 2 for Percent Passing");
+                Console.WriteLine("Press 3 for Odd Sum");
+                Console.WriteLine("Press 4 for Random Number");
+                Console.WriteLine("Press Q for Quit");
+                menuOption =Console.ReadLine();
+                if (menuOption == "1")
+                {
+                    Prompter();
+                }
+                else if (menuOption == "2")
+                {
+
+                }
+                else if (menuOption == "3")
+                {
+
+                }
+                else if (menuOption == "4")
+                {
+
+                }
+                else if (menuOption == "Q"||menuOption=="q")
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+        }
+        public static void Prompter()
+        {
+
+        }
+    }
+}
